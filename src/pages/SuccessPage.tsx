48ca27fcf5d7f83a5eaa6709234a71e27fce0120
CHARLIE <jcarlosbotero1@outlook.com>
2024-04-19 18:17:18 -0400

    Update SuccessPage.tsx

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -19,14 +19,14 @@ export default function SuccessPage() {
 				}}>
 				<br />
 				<div style={{ textAlign: 'center' }}>
-					<Typography level="h1">Lo has logrado</Typography>
+					<Typography level="h1">Operacion Exitosa</Typography>
 					<br />
-					<Typography level="title-lg">Hemos logrado desencriptar el codigo y encontramos la ubicacion de maveric</Typography>
+					<Typography level="title-lg">Hemos logrado desencriptar el código y encontramos la ubicación de Maverick.</Typography>
 					<br />
 					<Typography level="title-lg">23°26'06"N 75°47'51"W</Typography>
 					<br />
 					<Button component={Link} to="https://www.google.com/maps/place/23%C2%B026'06.0%22N+75%C2%B047'52.0%22W/@23.4351938,-75.8013552,16.5z/data=!4m4!3m3!8m2!3d23.435!4d-75.7977778?entry=ttu">
-						Ver ubicacion
+						Ver ubicación
 					</Button>
 				</div>
 				<br />
