f173fcdea47c8acd42242eb77db04b451d1cdea2
CHARLIE <jcarlosbotero1@outlook.com>
2024-04-19 17:33:29 -0400

    ...

--- src/pages/ConsoleAnimation.tsx
+++ src/pages/ConsoleAnimation.tsx
@@ -1,13 +1,23 @@
-/** @format */
-
 import * as React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import MatrixText from '../components/MatrixAnimation';
 
 export default function Home() {
-	return (
-		<React.Fragment>
-			<MatrixText />
-		</React.Fragment>
-	);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    // Wait for 15 seconds before redirecting
+    const timeoutId = setTimeout(() => {
+      navigate('/success');
+    }, 10000);
+
+    // Clean up the timeout
+    return () => clearTimeout(timeoutId);
+  }, [navigate]);
+
+  return (
+    <React.Fragment>
+      <MatrixText />
+    </React.Fragment>
+  );
 }
--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -2,24 +2,35 @@
 
 import * as React from 'react';
 import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
-
-// import { useNavigate } from 'react-router-dom';
-
-const containerStyle: React.CSSProperties = {
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	height: '100vh', // Set the height of the container to the viewport height
-};
+import { Link } from 'react-router-dom';
 
 export default function SuccessPage() {
 	return (
-		<Box sx={containerStyle}>
-			<br />
-			<Typography level="h1">Lo has logrado</Typography>
-			<Typography level="title-lg">Hemos logrado desencriptar el codigo y encontramos la ubicacion de maveric</Typography>
-			<br />
-		</Box>
+		<React.Fragment>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					height: '80vh',
+				}}>
+				<br />
+				<div style={{ textAlign: 'center' }}>
+					<Typography level="h1">Lo has logrado</Typography>
+					<br />
+					<Typography level="title-lg">Hemos logrado desencriptar el codigo y encontramos la ubicacion de maveric</Typography>
+					<br />
+					<Typography level="title-lg">23°26'06"N 75°47'51"W</Typography>
+					<br />
+					<Button component={Link} to="https://www.google.com/maps/place/23%C2%B026'06.0%22N+75%C2%B047'52.0%22W/@23.4351938,-75.8013552,16.5z/data=!4m4!3m3!8m2!3d23.435!4d-75.7977778?entry=ttu">
+						Ver ubicacion
+					</Button>
+				</div>
+				<br />
+			</Box>
+		</React.Fragment>
 	);
 }
